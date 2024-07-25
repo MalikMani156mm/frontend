7ae5efd68721b83d6740c823f9ff703a4c397243
MalikMani156.mm <malikmani156.mm@gmail.com>
2024-07-25 10:53:48 +0500

    completed

--- Backend/routes/PoliceStationRoutes.js
+++ Backend/routes/PoliceStationRoutes.js
@@ -9,8 +9,8 @@ import {
 import { isAuthenticatedUser, isAuthorizedUser } from '../middleware/authMiddleware.js';
 const router = express.Router()
 
-router.route('/PoliceStations').get(isAuthenticatedUser,isAuthorizedUser('SuperAdmin','Admin','Citizen'),getAllPoliceStations);
-router.route('/PoliceStation/:id').get(isAuthenticatedUser,isAuthorizedUser('SuperAdmin','Admin','Citizen'),getPoliceStationById);
+router.route('/PoliceStations').get(getAllPoliceStations);
+router.route('/PoliceStation/:id').get(getPoliceStationById);
 router.route('/new/PoliceStation').post(isAuthenticatedUser,isAuthorizedUser('SuperAdmin'),createNewPoliceStation);
 router.route('/updatePoliceStation/:id').put( isAuthenticatedUser,isAuthorizedUser('Admin'),updatePoliceStation);
 router.route('/deletePoliceStation/:id').delete(isAuthenticatedUser,isAuthorizedUser('SuperAdmin'), deletePoliceStation);
